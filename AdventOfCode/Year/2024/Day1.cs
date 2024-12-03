a66ff4ce289c47cb430aa101c8a20f40b8b12b97
Aaron <aaron@amorgan.net.nz>
2024-12-03 23:41:10 +1300

    AoC 2024, day 2, parts 1 and 2.

--- AdventOfCode/Year/2024/Day1.cs
+++ AdventOfCode/Year/2024/Day1.cs
@@ -7,14 +7,12 @@ public class Day1
     [Theory]
     [InlineData("Day1DevelopmentTesting1.txt", 11, 31)]
     [InlineData("Day1.txt", 2113135, 19097157)]
-    public void Day1_Part1_HistorianHysteria(string filename, int expectedAnswer, int expectedSimilarityScore)
+    public void Day1_Part1_Par2_HistorianHysteria(string filename, int expectedAnswer, int expectedSimilarityScore)
     {
-        var input = InputParser.ReadAllLines("2024/" + filename).ToArray();
+        var list1 = new List<int>();
+        var list2 = new List<int>();
 
-        var list1 = new List<int>(input.Length);
-        var list2 = new List<int>(input.Length);
-
-        foreach (var line in input)
+        foreach (var line in InputParser.ReadAllLines("2024/" + filename).ToArray())
         {
             var tmpStr = line.Split("   ");
 
@@ -25,24 +23,17 @@ public class Day1
         list1.Sort();
         list2.Sort();
 
-        int distance = 0, similarity = 0;
+        int distance = 0, similarityScore = 0;
 
         for (var i = 0; i < list1.Count; i++)
         {
-            if (list1[i] > list2[i])
-            {
-                distance += list1[i] - list2[i];
-            }
-            else
-            {
-                distance += list2[i] - list1[i];
-            }
+            distance += Math.Abs(list1[i] - list2[i]);
 
             // Part 2, multiple the number in list1 by it's number of occurrences in list2.
-            similarity += list1[i] * list2.FindAll(x => x == list1[i]).Count;
+            similarityScore += list1[i] * list2.FindAll(x => x == list1[i]).Count;
         }
 
         Assert.Equal(expectedAnswer, distance);
-        Assert.Equal(expectedSimilarityScore, similarity);
+        Assert.Equal(expectedSimilarityScore, similarityScore);
     }
 }
---
+++ AdventOfCode/Year/2024/Day2.cs
@@ -0,0 +1,81 @@
+﻿using Common.Utilities;
+
+namespace AdventOfCode.Year._2024;
+
+public class Day2
+{
+    [Theory]
+    [InlineData("Day2DevelopmentTesting1.txt", 2)]
+    [InlineData("Day2.txt", 591)]
+    public void Day2_Part1_Red_Nosed_Reports(string filename, int expectedAnswer)
+    {
+        var reports = InputParser.ReadAllLines("2024/" + filename).ToList();
+        var count = 0;
+
+        foreach (var report in reports)
+        {
+            var levels = report.Split().Select(int.Parse).ToList();
+
+            if (IsSafe(levels)) count++;
+        }
+
+        Assert.Equal(expectedAnswer, count);
+    }
+
+    [Theory]
+    [InlineData("Day2DevelopmentTesting1.txt", 4)]
+    [InlineData("Day2.txt", 621)]
+    public void Day2_Part2_Red_Nosed_Reports(string filename, int expectedAnswer)
+    {
+        var input = InputParser.ReadAllLines("2024/" + filename).ToList();
+        var count = 0;
+
+        foreach (var line in input)
+        {
+            var levels = line.Split().Select(int.Parse).ToList();
+
+            if (IsSafeWithTolerance(levels)) count++;
+        }
+
+        Assert.Equal(expectedAnswer, count);
+
+        return;
+
+        bool IsSafeWithTolerance(List<int> levels)
+        {
+            for (var i = 0; i < levels.Count; i++)
+            {
+                var tmpList = new List<int>(levels);
+
+                tmpList.RemoveAt(i);
+
+                if (IsSafe(tmpList)) return true;
+            }
+
+
+            return false;
+        }
+    }
+
+    bool IsSafe(List<int> levels)
+    {
+        bool ascending = levels[1] > levels[0];
+
+        for (var i = 1; i < levels.Count; i++)
+        {
+            var diff = Math.Abs(levels[i] - levels[i - 1]);
+            var asc = levels[i] > levels[i - 1];
+
+            if (ascending != asc) return false; // We've changed from asc to desc or vice versa.
+
+            if (diff is > 0 and <= 3)
+            {
+                continue;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+}
