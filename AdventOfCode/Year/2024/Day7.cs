c0821491126ae91574cec0471dd7a48794af75d3
Aaron <aaron@amorgan.net.nz>
2024-12-31 21:49:15 +1300

    AoC 2024, day 7, part 2.

--- AdventOfCode/Year/2024/Day7.cs
+++ AdventOfCode/Year/2024/Day7.cs
@@ -1,13 +1,16 @@
-﻿using Common.Utilities;
+﻿using Common.Types;
+using Common.Utilities;
 
 namespace AdventOfCode.Year._2024;
 
 public class Day7
 {
     [Theory]
-    [InlineData("Day7DevelopmentTesting1.txt", 3749)]
-    [InlineData("Day7.txt", 2654749936343)]
-    public void Day7_Part1_Bridge_Repair(string filename, long expectedAnswer)
+    [InlineData("Day7DevelopmentTesting1.txt", 3749, Part.One)]
+    [InlineData("Day7DevelopmentTesting1.txt", 11387, Part.Two)]
+    [InlineData("Day7.txt", 2654749936343, Part.One)]
+    [InlineData("Day7.txt", 124060392153684, Part.Two)]
+    public void Day7_Part1_Bridge_Repair(string filename, long expectedAnswer, Part part)
     {
         List<Equation> equations = [];
         Dictionary<int, List<char[]>> operandCombinations = new Dictionary<int, List<char[]>>();
@@ -30,15 +33,20 @@ public class Day7
         {
             if (!operandCombinations.ContainsKey(equation.Operands.Length))
             {
-                CalculateOperandCombinations(equation.Operands.Length);
+                if (part == Part.One)
+                {
+                    Part1CalculateOperandCombinations(equation.Operands.Length);
+                }
+                else
+                {
+                    Part2CalculateOperandCombinations(equation.Operands.Length);
+                }
             }
 
             var combinations = operandCombinations[equation.Operands.Length];
 
-            for (var j = 0; j < combinations.Count; j++)
+            foreach (var combination in combinations)
             {
-                var combination = combinations[j];
-
                 long sum = equation.Operands[0];
                 int index = 0;
 
@@ -63,19 +71,45 @@ public class Day7
 
         return;
 
-        void CalculateOperandCombinations(int operands)
+        // Creates operator combinations using binary, so we can represent two states and simply increment
+        // an integer counter to get the next combination.
+        void Part1CalculateOperandCombinations(int operands)
         {
             // 2 operations (n) possible per slot (s) is n to the power of s, e.g. 2 to the power of 3 = 8.
             int maxOperatorCombinationCount = (int)Math.Pow(2, operands - 1);
 
             List<char[]> combinations = [];
 
-
             for (int i = 0; i < maxOperatorCombinationCount; i++)
             {
+                // Create a base 2 representation of the operator combination using an incrementing integer as the seed.
                 string binary = Convert.ToString(i, 2).PadLeft(operands - 1, '0'); // Creates '010' e.g.
 
                 char[] binaryArray = binary.ToCharArray();
+
+                combinations.Add(binaryArray);
+            }
+
+            // Cache the combination under the key of number of operands.
+            operandCombinations[operands] = combinations;
+        }
+
+        // Creates operator combinations using binary, so we can represent two states and simply increment
+        // an integer counter to get the next combination.
+        void Part2CalculateOperandCombinations(int operands)
+        {
+            // 3 operations (n) possible per slot (s) is n to the power of s, e.g. 2 to the power of 3 = 8.
+            int maxOperatorCombinationCount = (int)Math.Pow(3, operands - 1);
+
+            List<char[]> combinations = [];
+
+            for (int i = 0; i < maxOperatorCombinationCount; i++)
+            {
+                // Create a base 3 (ternary) representation of the operator combination using an incrementing integer
+                // as the seed.
+                string ternary = ToBase3(i).PadLeft(operands - 1, '0'); // Creates '020' e.g.
+
+                char[] binaryArray = ternary.ToCharArray();
 
                 combinations.Add(binaryArray);
             }
@@ -91,14 +125,33 @@ public class Day7
             {
                 '0' => o1 + o2,
                 '1' => o1 * o2,
+                '2' => long.Parse(o1.ToString() + o2),    
                 _ => throw new ArgumentOutOfRangeException(nameof(operatorBit))
             };
+        }
+        
+        // Converts in integer to base 3 (ternary) returning the value as a string.
+        string ToBase3(int number)
+        {
+            if (number == 0) return "0";
+    
+            var digits = new List<int>();
+            int n = Math.Abs(number);
+    
+            while (n > 0)
+            {
+                digits.Add(n % 3);
+                n /= 3;
+            }
+    
+            digits.Reverse();
+            return (number < 0 ? "-" : "") + string.Join("", digits);
         }
     }
 
     private class Equation
     {
-        public required long Answer { get; set; }
-        public required long[] Operands { get; set; }
+        public required long Answer { get; init; }
+        public required long[] Operands { get; init; }
     }
 }
