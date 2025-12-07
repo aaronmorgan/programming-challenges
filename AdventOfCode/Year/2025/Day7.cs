3aa434d4b0cd7cbe9f1e0654be4f5ea7185092dd
Aaron Morgan <amorgan.nz@gmail.com>
2025-12-07 18:51:06 +1300

    AoC 2025, day 7, part 1.

---
+++ AdventOfCode/Year/2025/Day7.cs
@@ -0,0 +1,19 @@
+﻿using Common.Utilities;
+
+namespace AdventOfCode.Year._2025;
+
+public class Day7
+{
+    [Theory]
+    [InlineData("Day7DevelopmentTesting1.txt", 4277556)]
+    //[InlineData("Day7.txt", 4878670269096)]
+    public void Day6_Part1_Trash_Compactor(string filename, long expectedAnswer)
+    {
+        string[,] input = InputParser.ReadSpaceSeparatedFile($"2025/{filename}");
+        long result = 0;
+
+
+        Assert.Equal(expectedAnswer, result);
+    }
+
+}
